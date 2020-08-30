def7a1f26e59da562ff123370b01687f6f63d385
Trung Doan <doanptand@gmail.com>
2020-08-30 18:50:46 +0700

    register broadcast in java code

--- app/src/main/java/com/ddona/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/ddona/broadcastreceiver/MainActivity.java
@@ -2,13 +2,35 @@ package com.ddona.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import com.ddona.broadcastreceiver.receiver.MyReceiver;
+
 public class MainActivity extends AppCompatActivity {
+    private MyReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receiver = new MyReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction("com.ddona.myaction");
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 }
